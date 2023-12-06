41faaa134532d3f7befa0442cb7c624b542a3047
francois.lombard <francois.lombard@zenseact.com>
2023-12-06 13:45:39 +0200

    ✨ Add solution 2/2 day 03

--- day_03/solution.py
+++ day_03/solution.py
@@ -38,6 +38,15 @@ class Engine:
                     ret_val += self._get_surrounding_sum(x, y)
         return ret_val
 
+    def get_gear_ratio(self) -> int:
+        """Returns the gear ratio. Bad solution - It will not work for overlapping gears."""
+        ret_val = 0
+        for y, line in enumerate(self.lines):
+            for x, char in enumerate(line):
+                if not char.isalnum() and char != '.':
+                    ret_val += self._get_surrounding_product(x, y)
+        return ret_val
+
     def _get_and_replace_number(self, x: int, y: int) -> int:
         """Search East and grab from West."""
         extend_search = 0
@@ -64,12 +73,29 @@ class Engine:
 
         return ret_val
 
+    def _get_surrounding_product(self, x: int, y: int) -> int:
+        counter = 0
+        ret_val = 1
+        for dy, dx in DIRECTIONS:
+            ye = self._clamp(y + dy, len(self.lines))
+            xe = self._clamp(x + dx, len(self.lines[ye]))
+            if self.lines[ye][xe].isdigit():
+                ret_val *= self._get_and_replace_number(xe, ye)
+                counter += 1
+        if counter != 2:
+            return 0
+
+        return ret_val
+
     def _clamp(self, value: int, upper_bound: int, lower_bound: int = 0) -> int:
         return min(max(value, lower_bound), upper_bound)
 
 
 if __name__ == '__main__':
     assert Engine("example_01.txt").get_part_number_sum() == 4361
+    assert Engine("example_02.txt").get_gear_ratio() == 467835
 
     puzzle_result = Engine("puzzle_input.txt")
     print(f"Part number sum for puzzle 01: {puzzle_result.get_part_number_sum()}")
+    puzzle_result = Engine("puzzle_input.txt")
+    print(f"Gear ratio sum for puzzle 02: {puzzle_result.get_gear_ratio()}")
