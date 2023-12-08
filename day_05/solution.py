0de64c965f3a1bc19e35b16ddf629c45e8bedf5e
Francois <fnlombard@gmail.com>
2023-12-08 08:29:36 +0200

    ⚡️ Improve solution 1/2 day 05

--- day_05/solution.py
+++ day_05/solution.py
@@ -7,47 +7,44 @@ Usage: solution.py
 
 
 from pathlib import Path
-from typing import List
+from typing import Dict, Tuple
 
 
 class Almanac:
     """Loads in a scratch card number with its solution"""
 
     def __init__(self, filename: str) -> None:
-        self.chunks: List[str] = Path(filename).read_text('utf-8').strip().split('\n\n')
-        self.seeds = [
-            int(seed)
-            for seed in self.chunks.pop(0).split(':')[1].split()
-        ]
+        file_contents = Path(filename).read_text('utf-8').strip().split('\n\n')
+        self.seeds = [int(seed) for seed in file_contents.pop(0).split(':')[1].split()]
+        self.chunks = {}
+        for chunk in file_contents:
+            data = chunk.split('\n')
+            self.chunks[data[0]] = data[1:]
+        self.mapping_cache: Dict[Tuple[str, str, int], int] = {}
 
     def lowest_location_number(self) -> int:
         """Returns the lowest location number"""
-        lowest_value = float('inf')
-        for seed in self.seeds:
-            soil = self._map_value("seed", "soil", seed)
-            fertilizer = self._map_value("soil", "fertilizer", soil)
-            water = self._map_value("fertilizer", "water", fertilizer)
-            light = self._map_value("water", "light", water)
-            temperature = self._map_value("light", "temperature", light)
-            humidity = self._map_value("temperature", "humidity", temperature)
-            location = self._map_value("humidity", "location", humidity)
-            if location < lowest_value:
-                lowest_value = location
-        return lowest_value
+        return min(self._calculate_location(seed) for seed in self.seeds)
+
+    def _calculate_location(self, seed: int) -> int:
+        stages = [
+            "seed", "soil", "fertilizer", "water", "light", "temperature", "humidity", "location"
+        ]
+        for i in range(len(stages) - 1):
+            seed = self._map_value(stages[i], stages[i + 1], seed)
+        return seed
 
     def _map_value(self, from_type: str, to_type: str, value: int) -> int:
-        for chunk in self.chunks:
-            lines = chunk.split('\n')
-            if not lines.pop(0).startswith(f'{from_type}-to-{to_type}'):
-                continue
-            for line in lines:
-                values = line.split()
-                destination_start = int(values[0])
-                source_start = int(values[1])
-                value_range = int(values[2])
+        if (from_type, to_type, value) in self.mapping_cache:
+            return self.mapping_cache[(from_type, to_type, value)]
+        for line in self.chunks.get(f'{from_type}-to-{to_type} map:', []):
+            dest_start, source_start, value_range = map(int, line.split())
+            if source_start <= value <= source_start + value_range:
+                result = dest_start + value - source_start
+                self.mapping_cache[(from_type, to_type, value)] = result
+                return result
 
-                if source_start <= value <= source_start + value_range:
-                    return destination_start + value - source_start
+        self.mapping_cache[(from_type, to_type, value)] = value
         return value
 
 
