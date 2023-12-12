857055b4098190ef053ae378dec7141c503411a2
Francois <fnlombard@gmail.com>
2023-12-12 19:30:26 +0200

    🚧 WIP day 5

--- day_05/solution.py
+++ day_05/solution.py
@@ -9,7 +9,7 @@ Usage: solution.py
 from enum import Enum
 from pathlib import Path
 import re
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from dataclasses import dataclass
 
 
@@ -31,6 +31,9 @@ class MapInformation:
     source_range_start: int
     range_length: int
 
+    def __repr__(self) -> str:
+        return f"{self.source_range_start}-{self.desintation_range_start}-{self.range_length}"
+
 
 class RangeMap:
     """Store map in memory for quicker access"""
@@ -47,14 +50,26 @@ class RangeMap:
             )
         self._maps.sort(key=lambda item: item.source_range_start)
 
-    def map_value(self, value: int) -> int:
+    def map_value(self, source_value: int) -> int:
         """Returns mapped value"""
+        destination_value = source_value
+        range_to_end = 1
+
         for index, map_info in enumerate(self._maps):
-            if map_info.source_range_start <= value < map_info.source_range_start + map_info.range_length:
-                return map_info.desintation_range_start + value - map_info.source_range_start
-            elif index + 1 < len(self._maps) and value < self._maps[index + 1].source_range_start:
-                return value
-        return value
+            if source_value < map_info.source_range_start:
+                range_to_end = self._maps[index].source_range_start - source_value
+                break
+            elif map_info.source_range_start <= source_value < map_info.source_range_start + map_info.range_length:
+                destination_value = map_info.desintation_range_start + source_value - map_info.source_range_start
+                range_to_end = map_info.range_length - (map_info.source_range_start - source_value)
+                range_to_end /= 2
+                break
+            elif index + 1 < len(self._maps) and source_value < self._maps[index + 1].source_range_start:
+                destination_value = source_value
+                range_to_end = self._maps[index + 1].source_range_start - source_value
+                break
+
+        return (destination_value, range_to_end)
 
     def __repr__(self) -> str:
         ret_val = ""
@@ -69,20 +84,18 @@ class Almanac:
     def __init__(self, filename: str) -> None:
         file_contents = Path(filename).read_text('utf-8').strip().split('\n\n')
         self.seeds = [int(seed) for seed in file_contents.pop(0).split(':')[1].split()]
-        # self.chunks = {}
         self._range_maps: Dict[str, RangeMap] = {}
         for chunk in file_contents:
             data = chunk.split('\n')
             match = re.search(r'(\w+-to-\w+) map:', data.pop(0))
             key = match.group(1)
             self._range_maps[key] = RangeMap(data)
-            # self.chunks[data[0]] = data[1:]
 
     def lowest_location_number(self) -> int:
         """Returns the lowest location number"""
         min_location = float('inf')
         for seed in self.seeds:
-            test_value = self._calculate_location(seed)
+            test_value, _ = self._calculate_location_and_range(seed)
             if test_value < min_location:
                 min_location = test_value
         return min_location
@@ -90,20 +103,28 @@ class Almanac:
     def lowest_location_number_in_range(self) -> int:
         """Returns the lowest location number in seed range"""
         min_location = float('inf')
-        for seed_start, seed_range in zip(self.seeds[::2], self.seeds[1::2]):
-            for seed in range(seed_start, seed_start + seed_range):
-                test_location = self._calculate_location(seed)
+        seed_ranges = list(zip(self.seeds[::2], self.seeds[1::2]))
+        seed_ranges.sort(key=lambda value: value[0])
+        for seed_start, seed_range in seed_ranges:
+            seed = seed_start
+            while seed < seed_start + seed_range:
+                print(seed)
+                (test_location, test_range) = self._calculate_location_and_range(seed)
                 if test_location < min_location:
                     min_location = test_location
+                seed += test_range
         return min_location
 
-    def _calculate_location(self, seed: int) -> int:
+    def _calculate_location_and_range(self, seed: int) -> Tuple[int, int]:
         stages = [
             "seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"
         ]
+        min_range = float('inf')
         for stage in stages:
-            seed = self._range_maps[stage].map_value(seed)
-        return seed
+            (seed, range) = self._range_maps[stage].map_value(seed)
+            if range < min_range:
+                min_range = range
+        return (seed, min_range)
 
 
 if __name__ == '__main__':
@@ -112,4 +133,4 @@ if __name__ == '__main__':
 
     puzzle_result = Almanac("puzzle_input.txt")
     print(f"Lowest location value for puzzle 01: {puzzle_result.lowest_location_number()}")
-    print(f"Lowest location value in seed range for puzzle 01: {puzzle_result.lowest_location_number_in_range()}")
+    print(f"Lowest location value in seed range for puzzle 02: {puzzle_result.lowest_location_number_in_range()}")
