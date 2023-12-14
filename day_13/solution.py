1e00420c80af153b7f05efc8aa709c6b1cb66410
Francois <fnlombard@gmail.com>
2023-12-14 16:36:30 +0200

    ⚡️ Improve code day13.2

--- day_13/solution.py
+++ day_13/solution.py
@@ -5,108 +5,67 @@ Solution to https://adventofcode.com/2023/day/13
 Usage: solution.py
 """
 
-from dataclasses import dataclass
 from enum import Enum, auto
 from pathlib import Path
-from typing import List
-
+from typing import List, NamedTuple
 import numpy as np
 
 
 class Direction(Enum):
-    """Describes matrix reflection orientation"""
     VERTICAL = auto()
     HORIZONTAL = auto()
     UNKNOWN = auto()
 
 
-@dataclass
-class MatrixSignature:
-    """Used to compare relfection matrices"""
+class MatrixSignature(NamedTuple):
     reflection_line: int
     reflection_size: int
 
 
 class MirrorMaze:
-    """Contains maze information"""
-
     def __init__(self, raw_maze: List[str]) -> None:
         if not raw_maze:
             raise ValueError("raw_maze cannot be empty")
 
-        row_length = len(raw_maze[0])
-        maze_list = []
-        for line in raw_maze:
-            if len(line) != row_length:
-                raise RuntimeError("Inconsistent row lengths in the maze")
-            maze_list.append([self._char_to_int(char) for char in line])
-
-        self.maze = np.array(maze_list)
-
-    def _char_to_int(self, char: str) -> int:
-        if char == ".":
-            return 0
-        elif char == "#":
-            return 1
-        else:
-            raise RuntimeError(f"Unexpected character: {char}")
+        self.maze = np.array([
+            [1 if char == '#' else 0 for char in line]
+            for line in raw_maze
+        ])
 
     def get_reflection_checksum(self, tolerance: int) -> int:
-        """Get reflection checksum for the matrix"""
+        vert_sig = self._get_submatrix_signature(self.maze, tolerance)
+        hor_sig = self._get_submatrix_signature(np.rot90(self.maze), tolerance)
 
-        reflect_signature_vert, size_vert = self._get_submatrix_signature(self.maze, tolerance)
-
-        self.maze = np.rot90(self.maze)
-        reflect_signature_hor, size_hor = self._get_submatrix_signature(self.maze, tolerance)
-
-        if reflect_signature_vert == reflect_signature_hor == -1:
+        if vert_sig.reflection_line == hor_sig.reflection_line == -1:
             raise RuntimeError(f"No reflection found for matrix:\n{self.maze}")
 
-        if size_vert > size_hor:
-            return reflect_signature_vert
-        else:
-            return reflect_signature_hor * 100
+        return max(vert_sig.reflection_line, hor_sig.reflection_line * 100)
 
-    def _get_submatrix_signature(self, matrix: np.ndarray, tolerance) -> (int, int):
+    def _get_submatrix_signature(self, matrix: np.ndarray, tolerance) -> MatrixSignature:
         columns = matrix.shape[1]
-        max_window_size = columns if columns % 2 == 0 else columns - 1
-        min_window_size = 2
+        max_window_size = columns - (columns % 2)
 
-        for window_size in range(max_window_size, min_window_size - 1, -2):
-            # Must only test beginning and end:
-            submatrix = matrix[:, :window_size]
-            if self._is_matrix_symmetric(submatrix, tolerance):
-                return window_size // 2, window_size
-            submatrix = matrix[:, -window_size:]
-            if self._is_matrix_symmetric(submatrix, tolerance):
-                return columns - window_size // 2, window_size
-        return -1, -1
+        for window_size in range(max_window_size, 1, -2):
+            if self._is_matrix_symmetric(matrix[:, :window_size], tolerance):
+                return MatrixSignature(window_size // 2, window_size)
+            if self._is_matrix_symmetric(matrix[:, -window_size:], tolerance):
+                return MatrixSignature(columns - window_size // 2, window_size)
+        return MatrixSignature(-1, -1)
 
     def _is_matrix_symmetric(self, matrix: np.ndarray, tolerance) -> bool:
-        columns = matrix.shape[1]
-        if columns % 2 != 0:
-            raise RuntimeError("Cannot determine if matrix of uneven columns are vertically semitrical.")
-        left = matrix[:, columns // 2:]
-        right = np.fliplr(matrix[:, :columns // 2])
-        return np.count_nonzero(np.absolute((np.subtract(left, right))) == 1) == tolerance
+        # Splitting the matrix in twine results in a balanced tuple unpacking.
+        # pylint: disable-next=unbalanced-tuple-unpacking
+        left, right = np.split(matrix, 2, axis=1)
+        return np.count_nonzero(left != np.fliplr(right)) == tolerance
 
 
 class Solution:
-    """Loads in the Mirror Maze and processes it."""
-
     def __init__(self, filename: str) -> None:
-        file_contents: List[str] = Path(filename).read_text('utf-8').strip().split('\n\n')
-        self.mazes: List[MirrorMaze] = []
-        for content in file_contents:
-            self.mazes.append(MirrorMaze(content.split()))
+        file_contents = Path(filename).read_text('utf-8').strip().split('\n\n')
+        self.mazes = [MirrorMaze(content.split('\n')) for content in file_contents]
 
     def get_maze_checksum(self, tolerance: int = 0) -> int:
-        """Solution for puzzle 01"""
-        checksum = 0
-        for index, maze in enumerate(self.mazes):
-            test = maze.get_reflection_checksum(tolerance)
-            checksum += test
-        return checksum
+        return sum(maze.get_reflection_checksum(tolerance) for maze in self.mazes)
 
 
 if __name__ == '__main__':
@@ -115,5 +74,4 @@ if __name__ == '__main__':
 
     puzzle_result = Solution("puzzle_input.txt")
     print(f"Solution 01: {puzzle_result.get_maze_checksum()}")
-    puzzle_result = Solution("puzzle_input.txt")
     print(f"Solution 02: {puzzle_result.get_maze_checksum(tolerance=1)}")
